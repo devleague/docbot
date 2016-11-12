7a9065fd7ae2a643bdbc96b7701efd46c1f9cd07
sgnl <rayrfarias@gmail.com>
2016-11-12 12:12:49 -1000

    adds gulpfile, yea!

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,28 +1,26 @@
 const gulp = require('gulp');
 
 const postcss = require('gulp-postcss');
+const concat = require('gulp-concat');
 const postcssEasyImport = require('postcss-easy-import');
-const postcssCssnext = require('postcss-cssnext');
 const postcssNested = require('postcss-nested');
-const rucksack = require('rucksack-css');
 
 // magic string paths
 const paths = {
   scssSrc: 'src/assets/css/app.css',
   scssSrcGlob: 'src/assets/css/**/*',
-  cssDist: 'dist/assets/css'
+  cssDist: 'public/css'
 };
 
 gulp.task('css', _ => {
   const processors = [
     postcssEasyImport({glob: true}),
-    rucksack,
-    postcssCssnext,
     postcssNested
   ];
 
   return gulp.src(paths.scssSrc)
     .pipe(postcss(processors))
+    .pipe(concat('app.css'))
     .pipe(gulp.dest(paths.cssDist));
 });
 
