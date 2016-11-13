380332ed3dcaef86a4eb6de357fff14f6382b8e1
Jay Won <jason@sudokrew.com>
2016-11-13 11:51:27 -1000

    Added styling of popup to match landing page
    - Cleaned up script
    - Force delay for status update with setTimeout
    - Add jQuery for easy fade effects

--- chrome-ext/index.js
+++ chrome-ext/index.js
@@ -12,41 +12,21 @@ function getCurrentTabUrl(callback) {
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    // callback(url);
     sendUrlToDocbot(url, callback);
   });
 }
 
 function sendUrlToDocbot(url, callback) {
-  // var data = JSON.stringify({
-  // "url": url
-  // });
-
   var data = "url="+url;
-
   var xhr = new XMLHttpRequest();
-  // xhr.withCredentials = true;
 
   xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4) {
-      console.log(this.responseText);
       callback();
     }
   });
@@ -58,12 +38,15 @@ function sendUrlToDocbot(url, callback) {
 }
 
 function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+  $('#topStatus').fadeOut();
+  $('#bottomStatus').html(statusText).fadeIn();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
-    renderStatus('Thank you for your kindness.');
+    setTimeout(function(){
+      renderStatus('...thank you for your kindness.');
+    }, 1000);
   });
 });
 
