de565c441b43420cc486cc964d0957757793319f
sgnl <rayrfarias@gmail.com>
2016-11-13 01:36:03 -1000

    ultimate refactor c-c-c-c-combo

--- server/public/assets/js/app.js
+++ server/public/assets/js/app.js
@@ -1,7 +1,18 @@
 
-$('.button-collapse').sideNav({
-  menuWidth: 240, // Default is 240
-  edge: 'left', // Choose the horizontal origin
-  closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  draggable: true // Choose whether you can drag to open on touch screens
+$('.pin-side-nav').each(function() {
+  var $this = $(this);
+  var $target = $('#' + $(this).attr('data-target'));
+  $this.pushpin({
+    top: $target.offset().top,
+    bottom: $target.offset().top + $target.outerHeight() - $this.height()
+  });
+});
+
+$(document).ready(function(){
+  $('.pin-side-nav').pushpin({
+    top: 100,
+    offset: 0
+  });
+
+   $('.scrollspy').scrollSpy({ offset: 500 });
 });
