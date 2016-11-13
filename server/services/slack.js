2365e6ed95752bda33ca3fc611c306e7e9a57c8e
Sean Nakamura <seanenakamura@gmail.com>
2016-11-13 03:42:31 -1000

    expands SlackServices
    - adds delegateAction method to handle slack slash-command text query
    - renames getDocuments to parseDocuments for better naming
    - return parseMany's result as a string of urls with line breaking

--- server/services/slack.js
+++ server/services/slack.js
@@ -1,6 +1,46 @@
 'use strict';
+const Promise = require('bluebird');
+const MongoService = require('./mongo');
 
-function getDocuments(data){
+function delegateAction(cmd){
+  return new Promise((resolve, reject) => {
+    const reg = /([a-zA-Z\s]+)a*(\d)(th|st|rd|nd)*/;
+    const nth = ['th', 'st', 'rd', 'nd'];
+    const actions = ['random', 'all']
+    const params = cmd.split(reg);
+
+    let keyword = "";
+    let numAction = 0;
+    let wordAction = "";
+
+    if (params.length === 1){
+      let query = params[0].toLowerCase().split(' ');
+      if(query.length === 1){
+        keyword = query[0];
+      }else if (query.length >= 2){
+        if (query[1].indexOf(actions) > -1 ){
+          keyword = query[0];
+          wordAction = query[1];
+        }else {
+          keyword = `${query[0]} ${query[1]}`;
+          wordAction = query[2];
+        }
+      }
+      return resolve(MongoService.getTopContentItemsByCountAndKeyword(keyword));
+    }else if (params.length > 1){
+      if (!params[3]){
+        keyword = params[1].trim();
+        numAction = Number(params[2]);
+        return resolve(MongoService.getTopContentItemsByCountAndKeyword(keyword, numAction))
+      }else if (nth.indexOf(params[3]) > -1){
+        // find the position of the links
+        return reject('Not Accessible Yet')
+      }
+    }
+  });
+}
+
+function parseDocuments(data){
   if (data.url){
     return parseSingle(data);
   }else if (data.length > 0){
@@ -10,6 +50,7 @@ function getDocuments(data){
   }
 }
 
+// private static functions
 function parseSingle(data) {
   return data.url;
 }
@@ -19,9 +60,10 @@ function parseMany(data) {
   for (var i = 0; i < data.length; i++){
     urls.push(data[i].url);
   }
-  return urls;
+  return urls.join('\n');
 }
 
 module.exports = {
-  getDocuments: getDocuments
+  delegateAction: delegateAction,
+  parseDocuments: parseDocuments
 }
